921207cdbf443301620277c51502cced300fe287
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-27 09:46:20 -0600

    loggin page built but for some reason I am getting a 403 on it.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [cred, setCred] = useState({
+    credentials: {
+      username: '',
+      password: ''
+    }
+  })
+
+  const handleChange = e => {
+    setCred({
+      credentials: {
+        ...cred.credentials,
+        [e.target.name]: e.target.value
+      }
+    })
+    console.log(cred)
+  }
+
+  const submitLogin = e => {
+    e.preventDefault();
+    console.log("what I'm passing",cred.credentials)
+    axiosWithAuth()
+    .post('/api/login', cred.credentails)
+    .then(async res => {
+      console.log("Login Response", res)
+      await (localStorage.setItem('token', JSON.stringify(res.data.payload)))
+    })
+    .catch(err => {
+      console.log("Login Error", err.response)
+    })
+  }
+  //looking at the server I think I need to make this an async call.
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <br/>
+      <form onSubmit={submitLogin}>
+        <label>Name: 
+          <input 
+            type="text"
+            name="username"
+            id="username"
+            value={cred.credentials.username}
+            onChange={handleChange}
+          />
+        </label>
+        <br/>
+        <label>Password:
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={cred.credentials.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br/>
+        <button>Log In</button>
+      </form>
     </>
   );
 };
